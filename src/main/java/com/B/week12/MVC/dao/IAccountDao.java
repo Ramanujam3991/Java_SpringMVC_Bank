e51f6f23e416b97a2f98bafa2fe20fc205545d55
Ramanujam3991 <ramanujam3991@gmail.com>
2021-12-08 15:48:01 -0500

    merge

--- src/main/java/com/B/week12/MVC/dao/IAccountDao.java
+++ src/main/java/com/B/week12/MVC/dao/IAccountDao.java
@@ -8,12 +8,8 @@ public interface IAccountDao {
 
 	Account getAccountDetails(Account account);
 
-<<<<<<< HEAD
 	Transaction getTransactionDetails(Transaction transaction);
 
-=======
-	Transaction getTransactionDetails(HashMap<String, String> dataMap);
 		
 	void processForexTransaction(Account account, double amount);
->>>>>>> 4932685bf76aad0c3f5153f79a4bfafeb8729a22
 }
